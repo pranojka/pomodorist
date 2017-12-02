68189a17204a75183db28badb07d89b9d4a59e74
pranojka <jovana.pranic@gmail.com>
2017-12-02 12:30:45 -0800

    javastartandstop

--- js/pomodorist.js
+++ js/pomodorist.js
@@ -1,1 +1,25 @@
+function Count() {
 
+    var minutes = 25;
+    var seconds = 00;
+    timer = setInterval(function() {
+
+        document.getElementById("seconds").innerHTML = seconds % 60;
+        document.getElementById("minutes").innerHTML = parseInt(minutes);
+        seconds--;
+        if (seconds < 1) {
+            seconds = 59;
+            minutes--;
+        }
+    }, 1000);
+
+
+
+
+}
+
+function stopTimer() {
+
+    clearInterval(timer);
+
+}
---
+++ run.bat
@@ -0,0 +1,2 @@
+cd c:\users\sinergija5\desktop\workspace\pomodorist
+live-server --port=80 --host=my.dev
