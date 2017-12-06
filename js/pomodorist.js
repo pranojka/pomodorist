161b5eb3a13abe167a8711f4f26fc9df246148ac
pranojka <jovana.pranic@gmail.com>
2017-12-05 17:18:35 -0800

    cmmitment

--- js/pomodorist.js
+++ js/pomodorist.js
@@ -1,5 +1,43 @@
 var minutes = 24;
 var seconds = 60;
+va
+
+function Count() {
+    // debugger
+    seconds--;
+    document.getElementById("seconds").innerHTML = seconds;
+    document.getElementById("minutes").innerHTML = minutes;
+
+    if (minutes >= 1 && seconds > 0) {
+
+    } else if (minutes >= 1 && seconds == 0) {
+        -seconds = 59;
+        minutes--;
+    } else if (minutes == 0 && seconds > 0) {
+        // ignore this for now ...
+
+    } else if (minutes == 0, seconds == 0) {
+
+        stopTimer() //  var audio = document.getElementById(alert)
+        // audio.play()
+        alert("END!");
+    }
+}
+
+function start() {
+    timer = setInterval(Count, 10);
+
+}
+
+function stopTimer() {
+    clearInterval(timer);
+}
+
+function resetTimer() {
+    location.reload();
+}
+var minutes = 24;
+var seconds = 60;
 var timer;
 
 function Count() {
@@ -10,22 +48,34 @@ function Count() {
 
     if (minutes >= 1 && seconds > 0) {
         // ignore this for now ...
+
     } else if (minutes >= 1 && seconds == 0) {
-        seconds = 59;
+        +seconds = 59;
         minutes--;
     } else if (minutes == 0 && seconds > 0) {
         // ignore this for now ...
-    } else if (minutes == 0, seconds == 0) {
 
+    } else if (minutes == 0, seconds == 0) {
+        +debugger
+
+        var audio = document.getElementById("alert");
+        audio.play()
         stopTimer()
-        //  var audio = document.getElementById(alert)
-        // audio.play()
-        alert("END!");
+
+
     }
 }
 
 function start() {
-    timer = setInterval(Count, 10);
+
+
+
+    if (minutes == 0, seconds == 0) {
+        resetTimer()
+    }
+
+    timer = setInterval(Count, 1000);
+
 
 }
 
